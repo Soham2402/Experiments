5f803d5ed77146b5023a7a096d0d7b990a585e45
Soham2402 <sohamhegdessj4@gmail.com>
2025-08-16 17:04:49 +0530

    remove unecessary reduntant files

--- bloomfilter/src/components/Array.tsx
+++
@@ -1,9 +0,0 @@
-import type { OutputText } from "../interface"
-
-export const Array : React.FC<OutputText> = ({text}) => {
-    return (
-        <div>
-            {text}
-        </div>
-    )
-}
